8a582c6cfde9d2dee9de742db2c1ad4735fbe9e3
Frode Nilsen <frode.nilsen@v.gno>
2016-11-06 11:25:59 +0100

    Initial commit

---
+++ Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Csla;
+
+namespace UwpApp
+{
+    [Serializable]
+    public sealed class Customer : BusinessBase<Customer>
+    {
+        public static readonly PropertyInfo<string> NameProperty = RegisterProperty<string>(c => c.Name);
+        public string Name
+        {
+            get { return GetProperty(NameProperty); }
+            set { SetProperty(NameProperty, value); }
+        }
+
+        public static readonly PropertyInfo<int> AgeProperty = RegisterProperty<int>(c => c.Age);
+        public int Age
+        {
+            get { return GetProperty(AgeProperty); }
+            set { SetProperty(AgeProperty, value); }
+        }
+
+        public static readonly PropertyInfo<DateTime?> DateOfDeathProperty = RegisterProperty<DateTime?>(c => c.DateOfDeath);
+        public DateTime? DateOfDeath
+        {
+            get { return GetProperty(DateOfDeathProperty); }
+            set { SetProperty(DateOfDeathProperty, value); }
+        }
+    }
+}
